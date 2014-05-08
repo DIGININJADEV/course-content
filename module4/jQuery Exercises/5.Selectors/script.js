35dabab9a8b0dfd3e642ceba4a726e61f8ee227b
DIGININJADEV <keiichi.lindley@gmail.com>
2014-05-07 19:31:36 -0600

    Completed Assignments 5 - 6
    Still too easy ;)

--- module4/jQuery Exercises/5.Selectors/script.js
+++ module4/jQuery Exercises/5.Selectors/script.js
@@ -1,1 +1,17 @@
+$(document).ready(function(){
+	console.log('Changing text color in second p');
+	$('.sandbox p:nth-child(2)').css('color','rgba(255,255,255,0.5');
+	
+	console.log('Changing background of first p');
+	$('.sandbox p:nth-child(2)').css('color','rgba(255,255,255,0.5');
+	
+	console.log('Removing third p');
+	$('.sandbox p:nth-child(3)').remove();
+	
+	console.log('Changing text color of fourth li');
+	$('.sandbox li:nth-child(4)').css('color','rgba(255,255,255,0.5');
+	
+	console.log('Changing text of second li to "I did it!"');
+	$('.sandbox li:nth-child(2)').text('I did it!');
+});
 
--- module4/jQuery Exercises/6.Iterating/script.js
+++ module4/jQuery Exercises/6.Iterating/script.js
@@ -1,1 +1,16 @@
+$(document).ready(function(){
+	$('.sandbox li').each(function(index){
+	if(oddOrEven(index) === 'even'){
+		$(this).css('color','black');	
+	}else{
+		$(this).css('color','gray');	
+	}
+	});
+	
+	function oddOrEven(x) {
+  	return ( x & 1 ) ? "odd" : "even";
+	}
+});
+
+
 
